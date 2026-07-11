4403f7d7c35665cd3f73e1767c3831842f441994
marigold <phelixgluy@gmail.com>
2026-07-11 15:09:21 +0600

    fix: incorrect win.zig types

--- src/win.zig
+++ src/win.zig
@@ -2,29 +2,29 @@
 //!
 //! Contains only types that are missing in `std.os.windows`.
 
-const zigWin = @import("std").os.windows;
+pub const zigWin = @import("std").os.windows;
 
-const LPDWORD = *zigWin.DWORD;
-const BOOL = c_int;
-const FALSE: BOOL = 0;
-const TRUE: BOOL = 1;
+pub const LPDWORD = *zigWin.DWORD;
+pub const BOOL = c_int;
+pub const FALSE: BOOL = 0;
+pub const TRUE: BOOL = 1;
 
-const WPARAM = zigWin.UINT;
-const LPARAM = WPARAM;
+pub const WPARAM = zigWin.UINT;
+pub const LPARAM = WPARAM;
 
-const STD_ERROR_HANDLE: zigWin.DWORD = @bitCast(@as(i32, -12));
-const PROCESS_CREATE_THREAD = 0x0002;
-const PROCESS_VM_OPERATION = 0x0008;
-const PROCESS_VM_WRITE = 0x0020;
+pub const STD_ERROR_HANDLE: zigWin.DWORD = @bitCast(@as(i32, -12));
+pub const PROCESS_CREATE_THREAD = 0x0002;
+pub const PROCESS_VM_OPERATION = 0x0008;
+pub const PROCESS_VM_WRITE = 0x0020;
 
-const MEM_RESERVE = 0x00002000;
-const MEM_COMMIT = 0x00001000;
-const PAGE_READWRITE = 0x04;
+pub const MEM_RESERVE = 0x00002000;
+pub const MEM_COMMIT = 0x00001000;
+pub const PAGE_READWRITE = 0x04;
 
-const DLL_PROCESS_ATTACH: zigWin.DWORD = 1;
-const DLL_PROCESS_DETACH: zigWin.DWORD = 0;
+pub const DLL_PROCESS_ATTACH: zigWin.DWORD = 1;
+pub const DLL_PROCESS_DETACH: zigWin.DWORD = 0;
 
-extern "kernel32" fn WriteFile(
+pub extern "kernel32" fn WriteFile(
     hFile: zigWin.HANDLE,
     lpBuffer: zigWin.LPCVOID,
     nNumberOfBytesToWrite: zigWin.DWORD,
@@ -32,35 +32,35 @@ extern "kernel32" fn WriteFile(
     lpOverlapped: ?*anyopaque,
 ) callconv(.winapi) BOOL;
 
-extern "kernel32" fn OpenProcess(
+pub extern "kernel32" fn OpenProcess(
     dwDesiredAccess: zigWin.DWORD,
     bInheritHandle: BOOL,
     dwProcessId: zigWin.DWORD,
 ) callconv(.winapi) ?zigWin.HANDLE;
 
-extern "kernel32" fn GetStdHandle(nStdHandle: zigWin.DWORD) callconv(.winapi) ?zigWin.HANDLE;
+pub extern "kernel32" fn GetStdHandle(nStdHandle: zigWin.DWORD) callconv(.winapi) ?zigWin.HANDLE;
 
-extern "user32" fn FindWindowExW(
+pub extern "user32" fn FindWindowExW(
     hWndParent: ?zigWin.HWND,
     hWndChildAfter: ?zigWin.HWND,
     lpClassName: zigWin.LPCWSTR,
     lpWindowName: ?zigWin.LPCWSTR,
 ) callconv(.winapi) ?zigWin.HWND;
 
-extern "user32" fn GetWindowThreadProcessId(
+pub extern "user32" fn GetWindowThreadProcessId(
     hwnd: zigWin.HWND,
     lpwdProcessId: *zigWin.DWORD,
 ) callconv(.winapi) zigWin.DWORD;
 
-extern "user32" fn VirtualAllocEx(
+pub extern "user32" fn VirtualAllocEx(
     hProcess: zigWin.HANDLE,
-    lpAddress: zigWin.LPVOID,
+    lpAddress: ?zigWin.LPVOID,
     dwSize: zigWin.SIZE_T,
     flAllocationType: zigWin.DWORD,
     flProtect: zigWin.DWORD,
 ) callconv(.winapi) ?zigWin.LPVOID;
 
-extern "kernel32" fn WriteProcessMemory(
+pub extern "kernel32" fn WriteProcessMemory(
     hProcess: zigWin.HANDLE,
     lpBaseAddress: zigWin.LPVOID,
     lpBuffer: zigWin.LPCVOID,
@@ -68,14 +68,14 @@ extern "kernel32" fn WriteProcessMemory(
     lpNumberOfBytesWritten: *zigWin.SIZE_T,
 ) callconv(.winapi) BOOL;
 
-extern "kernel32" fn CreateRemoteThread(
+pub extern "kernel32" fn CreateRemoteThread(
     hProcess: zigWin.HANDLE,
-    lpThreadAttributes: *zigWin.SECURITY_ATTRIBUTES,
+    lpThreadAttributes: ?*zigWin.SECURITY_ATTRIBUTES,
     dwStackSize: zigWin.SIZE_T,
-    lpStartAddress: zigWin.THREAD_START_ROUTINE,
+    lpStartAddress: *const zigWin.THREAD_START_ROUTINE,
     lpParameter: zigWin.LPVOID,
     dwCreationFlags: zigWin.DWORD,
-    lpThreadId: zigWin.LPDWORD,
+    lpThreadId: ?LPDWORD,
 ) callconv(.winapi) zigWin.HANDLE;
 
-extern "kernel32" fn LoadLibraryW(lpLibFileName: zigWin.LPCWSTR) callconv(.winapi) zigWin.HMODULE;
+pub extern "kernel32" fn LoadLibraryW(lpLibFileName: zigWin.LPCWSTR) callconv(.winapi) zigWin.HMODULE;
