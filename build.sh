3ee40e64e01a334c16db6cac0a99e999383aeb73
marigold <phelixgluy@gmail.com>
2026-07-11 18:57:09 +0600

    add: build.sh instead of buildExe.sh and buildDll.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 # Used for production builds
 # Receives one argument: compilation target for zig
+# Emits an archieve
 
 zig build-exe src/main.zig -target $1 -O ReleaseFast -fsingle-threaded \
 	-fno-unwind-tables -fstrip -femit-bin="zig-out/transparenZ.exe"
+
+zig build-lib src/main.zig -target $1 -dynamic -O ReleaseFast -fsingle-threaded \
+	-fno-unwind-tables -fstrip -femit-bin="zig-out/ui.dll"
+
+(cd zig-out && tar -caf zqjs-$1.tar.gz .)
--- buildDll.sh
+++
@@ -1,5 +0,0 @@
-# Used for production builds
-# Receives one argument: compilation target for zig
-
-zig build-lib src/main.zig -target $1 -dynamic -O ReleaseFast -fsingle-threaded \
-	-fno-unwind-tables -fstrip -femit-bin="zig-out/ui.dll"
