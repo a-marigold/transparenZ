20931d3fc47d8df917c64476eca4a343e5eff785
marigold <phelixgluy@gmail.com>
2026-07-12 13:16:13 +0600

    fix: name in build.zig.zon

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 # Used for production builds
-# Receives one argument: compilation target for zig
-# Emits an archieve
+# Receives two arguments: 
+# 1. compilation target for zig
+# 2. path to of archive to be outputed (e.g `transparenZ.tar.gz`s`)
 
 zig build-exe src/main.zig -target $1 -O ReleaseFast -fsingle-threaded \
 	-fno-unwind-tables -fstrip -femit-bin="zig-out/transparenZ.exe"
-
 zig build-lib src/main.zig -target $1 -dynamic -O ReleaseFast -fsingle-threaded \
 	-fno-unwind-tables -fstrip -femit-bin="zig-out/ui.dll"
-
-(cd zig-out && tar -caf zqjs-$1.tar.gz .)
+	tar -caf $2.tar.gz -C zig-out .
