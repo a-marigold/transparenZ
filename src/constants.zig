d434603fc7a6bca9a1691ee8c589a54b28f8b354
marigold <phelixgluy@gmail.com>
2026-07-16 22:08:16 +0600

    add: UiDllError enum with all errors of ui.dll in constants

--- src/constants.zig
+++ src/constants.zig
@@ -1,9 +1,30 @@
 const std = @import("std");
-
 const unicode = std.unicode;
-
 const zigWin = std.os.windows;
+
+const win = @import("win.zig");
 
 pub const UI_DLL_FILE_NAME = "ui.dll";
 
 pub const UTF16_BACK_SLASH: zigWin.WCHAR = unicode.utf8ToUtf16LeStringLiteral("\\")[0];
+
+// const ACCENT_POLICY: win.ACCENT_POLICY = .{
+//     .AccentState = .ACCENT_ENABLE_ACRYLICBLURBEHIND,
+//     .AccentFlags = 0,
+//     .GradientColor = 0x010000,
+
+//     .AnimationId = 0,
+// };
+
+// pub const TASKBAR_COMPOSITION_ATTRIB_DATA: win.WINDOWCOMPOSITIONATTRIBDATA = .{
+//     .Attrib = .WCA_ACCENT_POLICY,
+//     .pvData = &ACCENT_POLICY,
+//     .cbData = @sizeOf(win.ACCENT_POLICY),
+// };
+
+/// Enumiration of errors that appear in the `ui.dll`.
+///
+/// Error is allocated in `explorer.exe`, written by `ui.dll` and read in the main process.
+pub const UiDllError = enum(u32) {
+    GetExeDirFailed,
+};
