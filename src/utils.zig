5fcf3613a50efddac47f948b60fd1e3abd1bd309
marigold <phelixgluy@gmail.com>
2026-07-26 15:30:55 +0600

    delete(utils.zig): getExeDirPath and exeDirPathToUiDllPath. add(utils.zig): getExePath

--- src/utils.zig
+++ src/utils.zig
@@ -6,62 +6,24 @@ const win = @import("win.zig");
 
 const constants = @import("constants.zig");
 
-pub const AbsPath = struct {
-    /// `buffer.len` does not represent the real length of path.
+/// Returns slice of `buffer` or `null` in case of error.
+pub inline fn getExePath(
+    /// Handle of a module (for example, from `LoadLibrary` function), exe path of which to return.
     ///
-    /// Use `AbsPath.len` instead.
-    ///
-    ///
-    /// Everything that is after `AbsPath.buffer[AbsPath.len - 1]` is a stack garbage.
-    buffer: [zigWin.MAX_PATH:0]zigWin.WCHAR,
+    /// `null` treated as current module handle.
+    module: ?zigWin.HMODULE,
+    /// Receives the result.
+    buffer: *[zigWin.MAX_PATH]u16,
+) ?[]u16 {
+    const pathLen = win.GetModuleFileNameW(module, buffer, buffer.len);
 
-    /// Length of path in `buffer`.
-    len: zigWin.DWORD,
-};
-
-/// Returns `AbsPath` struct or `null` in case of error.
-///
-/// Returned `len` **doesn't** include trailing backslash (`\`) of path char in `buffer`.
-///
-/// (it means `result.buffer[result.len - 1]` **doesn't** retrieves `\`).
-pub fn getExeDirPath() ?AbsPath {
-    var buffer: @FieldType(AbsPath, "buffer") = undefined;
-
-    const absPathLen = win.GetModuleFileNameW(null, &buffer, buffer.len);
-
-    if (absPathLen == 0) {
+    if (pathLen == 0) {
         @branchHint(.cold);
 
         return null;
     }
 
-    const utf16BackSlash: u16 = '\\';
-
-    var pathIndex = absPathLen - 1;
-
-    while (pathIndex > 0) : (pathIndex -= 1) {
-        if (buffer[pathIndex] == utf16BackSlash) {
-            break;
-        }
-    }
-
-    return .{ .buffer = buffer, .len = pathIndex };
-}
-
-/// Mutates `exeDirPath.buffer` via copying and appending `constants.UI_DLL_FILE_NAME` there.
-///
-/// Also appends Null Terminator to the path and includes it in `AbsPath.len`.
-///
-/// Example:
-///
-/// After function call `exeDirPath.buffer` contains `...\somePath\ui.dll\0`, and `exeDirPath.len` is updated.
-pub inline fn exeDirPathToUiDllPath(exeDirPath: *AbsPath) void {
-    const uiDllPath = comptime unicode.utf8ToUtf16LeStringLiteral("\\" ++ constants.UI_DLL_FILE_NAME);
-
-    @memcpy(exeDirPath.buffer[exeDirPath.len..], uiDllPath);
-
-    exeDirPath.len += uiDllPath.len + 1; // Add 1 for Null Terminator
-    exeDirPath.buffer[exeDirPath.len - 1] = 0; // Add Null Terminator
+    return buffer[0..pathLen];
 }
 
 /// Opens process which owns the window of `windowClassName`.
